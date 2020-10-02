76cb7e0924383740533d1b941b2074200656785e
Priyam Modak <priyam.inferno@gmail.com>
2020-10-03 00:50:17 +0530

    Done

---
+++ src/Config/firebaseConfig.js
@@ -0,0 +1,9 @@
+export default {
+    apiKey: "AIzaSyBFZhQkrymsan383fWTN37idrh3ojGCgao",
+    authDomain: "fir-authentication-dac7a.firebaseapp.com",
+    databaseURL: "https://fir-authentication-dac7a.firebaseio.com",
+    projectId: "fir-authentication-dac7a",
+    storageBucket: "fir-authentication-dac7a.appspot.com",
+    messagingSenderId: "455627114014",
+    appId: "1:455627114014:web:dd9515accb8a4199b3900f"
+};
---
+++ src/components/UserCard.js
@@ -0,0 +1,23 @@
+import Axios from "axios";
+import React, { useContext, useState } from "react";
+import { toast } from "react-toastify";
+import { Card, CardBody } from "reactstrap";
+import { UserContext } from "../context/UserContext";
+
+const UserCard = ({user}) => {
+
+    return (
+        <Card className="text-center mt-3 mb-4"> 
+            <img src={user.avatar_url} className="img-thumbnail" height="200px"/>
+            <CardBody>
+                <div className="text-primary">{user.name}</div>
+                <div className="text-primary">{user.location}</div>
+                <div className="text-primary">{user.bio}</div>
+                <div className="text-info">Availale for hire: {user.hirable?"Yup":"Nope"}</div>
+                <div className="text-info">Followers: {user.followers}</div>
+            </CardBody>
+        </Card>
+    )
+} 
+
+export default UserCard;
